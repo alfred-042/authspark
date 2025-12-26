ea480e5ea5af0adf98430ab883d5d165dc260d83
Clair Delune <delunec220@gmail.com>
2025-12-26 13:05:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5686891959-9ea1e",
+  "appId": "1:442514994966:web:6b2c0f7d799bfce5dac237",
+  "apiKey": "AIzaSyCjWZ8SIcNirB5nbBlozmdtoHtTnR81zS8",
+  "authDomain": "studio-5686891959-9ea1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "442514994966"
+};
